634f0172d240c82e433ce78286cefce7072183f2
Raistaid <147325049+Raistaid@users.noreply.github.com>
2025-02-28 19:28:07 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -4,24 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def mission():
+    return "Миссия Колонизация Марса"
+
+
 @app.route('/index')
 def index():
     return "И на Марсе будут яблони цвести!"
-
-
-@app.route('/sample_page')
-def return_sample_page():
-    return f"""<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Задание Яндекс лицей</title>
-                  </head>
-                  <body>
-                    <h1>Миссия Колонизация Марса</h1>
-                  </body>
-                </html>"""
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
