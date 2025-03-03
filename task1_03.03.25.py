24ec149f4d1968d93f813915b3fa4f6bf0ac716f
Raistaid <147325049+Raistaid@users.noreply.github.com>
2025-03-03 17:03:21 +0300

    Create task1_03.03.25.py

---
+++ task1_03.03.25.py
@@ -0,0 +1,12 @@
+from flask import Flask, url_for, request, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/index<title>')
+@app.route('/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
