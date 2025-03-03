48b85b86820aaf2c959c8705923c13e374cb011e
Raistaid <147325049+Raistaid@users.noreply.github.com>
2025-03-03 17:13:01 +0300

    Update task1_03.03.25.py

--- task1_03.03.25.py
+++ task1_03.03.25.py
@@ -3,7 +3,7 @@ from flask import Flask, url_for, request, render_template, redirect
 app = Flask(__name__)
 
 
-@app.route('/index<title>')
+@app.route('/index/<title>')
 @app.route('/<title>')
 def index(title):
     return render_template('base.html', title=title)
