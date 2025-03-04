f0ba80eac2bbcc1166f13e409778d489131b1b6c
Raistaid <147325049+Raistaid@users.noreply.github.com>
2025-03-04 23:08:21 +0300

    Update task5_27.02.25(отбор астронавтов).py

--- task5_27.02.25(отбор астронавтов).py
+++ task5_27.02.25(отбор астронавтов).py
@@ -118,3 +118,5 @@ def choosing():
                         </html>'''
     elif request.method == 'POST':
         return "Форма отправлена"
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
